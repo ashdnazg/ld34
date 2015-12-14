0548180b169279860fe3cdb8ac5bcf80b903a7c3
Ben Tyler <benjamin.tyler@gmail.com>
2015-12-14 18:03:29 +0100

    stats page, notification

--- lib/player.lua
+++ lib/player.lua
@@ -44,36 +44,50 @@ function Player:censorStation()
     local success, message = self.radio:censor()
     if success then
         self.censored = self.censored + 1
+        self:notify("Good work. " .. censorOutcomes[message])
         -- GOOD JOB PLAYER. highlight the rule that was violated?
         if self.censored == (#self.radio.stations - 1) then
             game:conclude("success")
         end
     else
-        -- TODO: per-channel censorship failure messages? like, how dare you
-        -- censor a beloved children's radio host
+        -- TISK TISK. BAD PLAYER: play nasty sound, flash a light, whatever.
+        -- show the message, bump the unrest meter + associated message
+        self:notify('WRONG')
         self.unrest = self.unrest + 1
         if self.unrest == #unrestText then
             game:conclude("failure")
         end
     end
-
-    print("tried  to censor: ", success, message, censorOutcomes[message])
 end
 
 -- all player monitors: successful/failed censors
 function Player:draw()
     local w, h = love.graphics.getWidth(), love.graphics.getHeight()
-    love.graphics.print(unrestText[self.unrest], w / 4.5, h / 1.5)
+    love.graphics.setColor(0, 0, 0, 255)
+    love.graphics.print("public status: " .. unrestText[self.unrest], 50, 70)
+    love.graphics.print("truthified: " .. self.censored .. "/" .. (#self.radio.stations - 1), 50, 100)
+    if self.notice then
+        love.graphics.print(self.notice, 50, 150)
+    end
+    love.graphics.setColor(255, 255, 255, 255)
     self.radio:draw()
 end
 
 function Player:update(dt)
+    if self.notice then
+        self.noticeTime = self.noticeTime + dt
+        if self.noticeTime > 3 then
+            self.notice = nil
+            self.noticeTime = 0
+        end
+    end
     self.radio:update(dt)
 end
 
 -- TODO: some way to send messages
-function Player:notify()
-
+function Player:notify(message)
+    self.notice = message
+    self.noticeTime = 0
 end
 
 function Player:conclude()
