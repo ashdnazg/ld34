2f2fdaff5761355ee1fe3441b0697a182f5b43a9
Ben Tyler <benjamin.tyler@gmail.com>
2015-12-14 21:46:57 +0100

    fade in/out for tutorial

--- lib/tutorial.lua
+++ lib/tutorial.lua
@@ -9,58 +9,132 @@ end
 
 function Tutorial:start()
     self.phase = 1
+
     self.phases = {
-        {},
         {
+            autoadvance = true,
+            sound = { vol = 0.5 },
+            color = { alpha = 255 },
+        },
+        {
+            autoadvance = false,
+            sound = { vol = 1 },
+            color = { alpha = 0 },
+        },
+        {
+            autoadvance = true,
+            sound = { vol = 1 },
+            color = { alpha = 0 },
+        },
+        {
+            autoadvance = true,
             sound = { vol = 0 },
             color = { alpha = 255 },
-        }
+        },
+        {
+            autoadvance = true,
+            sound = { vol = 0 },
+            color = { alpha = 0 },
+        },
+
     }
-    local conclude = self.phases[2]
-    conclude.colorTween = tween.new(3, conclude.color, { alpha = 0 }, 'linear')
-    conclude.soundTween = tween.new(3, conclude.sound, { vol = 1 }, 'linear')
+
+    self.phases[1].colorTween =
+        tween.new(1, self.phases[1].color, self.phases[2].color, 'linear')
+
+    self.phases[1].soundTween =
+        tween.new(1, self.phases[1].sound, self.phases[2].sound, 'linear')
+
+    self.phases[2].colorTween =
+        tween.new(1, self.phases[2].color, self.phases[3].color, 'linear')
+
+    self.phases[2].soundTween =
+        tween.new(1, self.phases[2].sound, self.phases[3].sound, 'linear')
+
+    self.phases[3].colorTween =
+        tween.new(1, self.phases[3].color, self.phases[4].color, 'linear')
+
+    self.phases[3].soundTween =
+        tween.new(0.1, self.phases[3].sound, self.phases[4].sound, 'linear')
+
+    self.phases[4].colorTween =
+        tween.new(1, self.phases[4].color, self.phases[5].color, 'linear')
+
+
+    self.source = love.audio.newSource("assets/audio/tutorial.ogg")
+    self.source:setVolume(1)
+    self.source:play()
+    self.timer = 0
+    self.voiceLength = 78.5
 end
 
 function Tutorial:draw()
     local w, h = love.graphics.getWidth(), love.graphics.getHeight()
-    if self.phase == 1 then
-        love.graphics.print("this is the tutorial, press 'e' to start the game", w / 3, h / 2.5)
-    elseif self.phase == 2 then
-        local phase = self.phases[self.phase]
-        love.graphics.setColor(255, 255, 255, 255)
-        love.graphics.draw(game.background)
+    local phase = self.phases[self.phase]
+    love.graphics.setColor(255, 255, 255, 255)
+    love.graphics.draw(game.background)
+
+    if phase.color then
         love.graphics.setColor(0, 0, 0, phase.color.alpha)
-        love.graphics.rectangle( "fill", 0, 0, w, h )
     end
+
+    love.graphics.rectangle( "fill", 0, 0, w, h )
 end
 
 function Tutorial:fadeOut()
-    self.phase = 2
+    if self.phase < 3 then
+        self.phase = 3
+    end
 end
 
 function Tutorial:update(dt)
-    if self.phase == 2 then
-        local phase = self.phases[2]
-        if phase.colorTween and phase.colorTween:update(dt) then
-            print("advancing game state to game!!")
-            self.advanceGameState()
-        end
+    self.timer = self.timer + dt
+    local phase = self.phases[self.phase]
+    local phaseDone = phase.autoadvance
+    if phase.colorTween then
+        --print("phase ", self.phase, "and colorTween", phase.color.alpha)
+        local tweenDone = phase.colorTween:update(dt)
+        phaseDone = phaseDone and phase.colorTween:update(dt)
+    end
+
+    if phase.soundTween then
+        print("phase ", self.phase, "and colorTween", phase.sound.vol)
+        self.source:setVolume(phase.sound.vol)
+        phaseDone = phaseDone and phase.soundTween:update(dt)
+    end
+
+    if self.phase == 5 then
+        print("advancing game state to game!!")
+        self.source:stop()
+        self.advanceGameState()
+    end
+
+    if phaseDone and self.phase < #self.phases then
+        self.phase = self.phase + 1
+    end
+
+    if self.timer > self.voiceLength and self.phase < 3 then
+        self.phase = 3
     end
 end
 
 function Tutorial:handleKey(key)
-    if key == 'e' then
-        self:conclude()
+    if key == ' ' then
+        if not self.concluding then
+            self:conclude()
+        end
     end
 end
 function Tutorial:mousePressed(x, y, key)
 end
 
 function Tutorial:handleMouse(x, y, key)
+
 end
 
 -- clean stuff up
 function Tutorial:conclude()
+    self.concluding = true
     print("tutorial is ending!")
     self:fadeOut()
 end
